06b04b0e16de7041e9b1b06662833b20a34ee8d0
Ashish Keshri <keshri.ashish1@gmail.com>
2022-12-14 11:15:29 +0530

    footer-ui-changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 const Footer = () => {
+
+  const [backToTopButton, setBackToTopButton] = useState(false);
+
   let year = new Date().getFullYear();
+
+  useEffect(() => {
+
+    window.addEventListener(('scroll'), () => {
+
+      window.scrollY > 100 ? setBackToTopButton(true) : setBackToTopButton(false)
+
+    })
+
+
+  }, []);
+  
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
 
   return (
     <>
       <div className="py-2 bg-light">
         <div className="container">
-          <p className="d-flex justify-content-center">&copy; Ashish Keshri {year}</p>
+          <p className="d-flex justify-content-center">
+            &copy; Ashish Keshri {year}
+          </p>
+          <div className="position-fixed bottom-0 end-0 m-3">
+          { backToTopButton && (
+            <button className="btn btn-dark rounded-circle" onClick={() => scrollUp()}><i className="fa-solid fa-arrow-up"></i></button>
+          )
+           } 
+
+          </div>
         </div>
       </div>
     </>
